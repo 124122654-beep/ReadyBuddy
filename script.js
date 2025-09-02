aa4f43e98dff08f73a3a0c2c40014c4f7d8fe8e0
124122654-beep <124122654@umail.ucc.ie>
2025-09-02 14:10:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add smooth scrolling and interactive features
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
